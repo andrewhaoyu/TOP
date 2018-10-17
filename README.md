ea5b0d040d566bcae51e810b99190f7dd89e22ac
andrewhaoyu <andrew.haoyu@gmail.com>
2018-10-16 23:50:48 -0700

    version 1.0.8 tutorial draft finished

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# TOP
-Two-stage polytomous logistic regression
+TOP
+=======
+
+Two-stage polytomous logistic regression (TOP): in this package, we developed a two-stage polytomous regression framework to handle cancer data with multivariate tumor characteristics. In the first stage, a standard polytomous model is used to specify for all subtypes defined by the cross-classification of different markers. In the second stage, the subtype-specific case-control odds ratios are specified using a more parsimonious model based on the case-control odds ratio for a baseline subtype, and the case-case parameters associated with tumor markers. Further, to reduce the degrees-of-freedom, we allow to specify case-case parameters for additional markers using a random-effect model.
+Usage
+=======
+
+[The 'TOP' vignette](https://github.com/andrewhaoyu/CKLRT/blob/master/inst/CKLRT_package.pdf) will provide a good start point for using TOP package.
+
+
+Development 
+=======
+This R package is developed by Haoyu Zhang and William Wheeler, and maintained by Haoyu Zhang <andrew.haoyu@gmail.com>.
+
+Installation
+=======
+To install the development version of TOP, it's easiest to use the 'devtools' package.
+
+install.packages("devtools")  
+library(devtools)  
+install_github("andrewhaoyu/TOP")
+
+References
+=======
+Zhang, H., Zhao, N., Ahearn, T.U, Wheeler W., García-Closas, M., Chatterjee, N., A mixed-model approach for powerful testing of genetic associations with cancer risk incorporating tumor characteristics (Submitted)
+
+
--- tests/test_example.R
+++ tests/test_example.R
@@ -84,7 +84,7 @@ PC1 <- data[,7,drop=F]
 
 model.3 <- EMmvpolySelfDesign(y,
           x.self.design = SNP,
-    z.design = z.deign,
+    z.design = z.design,
     additive=PC1,
   missingTumorIndicator = 888)
 
@@ -94,21 +94,90 @@ model.3 <- EMmvpolySelfDesign(y,
 
 
 
+#fit the two-stage model under the null hypothesis
+#that the second stage parameters of SNP is 0
+#the model only has one covariate PC1
+score.support.fixed <- ScoreTestSupportMixedModel(y=y,
+                                                  additive=PC1,
+                                                  missingTumorIndicator=888)
 
 
-############fit the additive two-stage model under the null hypothesis that the second stage parameters of SNP is 0
-score.support <- ScoreTestSupportMixedModel(y=y,
-                                       additive=PC1,
-                                  missingTumorIndicator=888)
+#Generate the additive second stage design matrix
+z.additive <- cbind(1,z.standard)
+#compute the score and information matrix for SNP
+score.test.fixed <- ScoreTestMixedModel(y=y,
+                                        x=SNP,
+                                        z.design = z.additive,
+                                        score.test.support=score.support.fixed,
+                                        missingTumorIndicator=888)
+#the first element is the score
+#the second element is the information matrix
+score.fixed <- score.test.fixed[[1]]
+infor.fixed <- score.test.fixed[[2]]
+#compute the global association test p value
+DisplayFixedScoreTestResult(score.fixed,infor.fixed) 
+
+
   
 
 
-  
+#we are going to build a two-stage model with 
+#baseline parameter and ER case-case parameter as fixed
+#We assume the PR, HER2, grade case-case parameter 
+#to be random
+#fit the two-stage model under the null hypothesis
+#that the second stage parameters of SNP is 0
+#the model only has one covariate PC1
+#Generate the z design matrix for fixed effect
+z.design.fixed <- cbind(1,z.standard[,1])
+#compute the score and information matrix for fixed effect
+score.test.fixed <- ScoreTestMixedModel(y=y,
+                    x=SNP,
+                    z.design=z.design.fixed,
+                    score.test.support=score.support.fixed,
+                    missingTumorIndicator=888)
+#the first element is the score
+#the second element is the information matrix
+score.fixed <- score.test.fixed[[1]]
+infor.fixed <- score.test.fixed[[2]]
 
+#fit the two-stage model under the null hypothesis
+#that only the random effect is 0
+#the model will have two covariates, 
+#PC1 and the fixed effect of SNP
+score.support.random <- ScoreTestSupportMixedModelSelfDesign(y=y,
+                        x.self.design  = SNP,
+                        z.design = z.design.fixed,
+                        additive = PC1,
+                        missingTumorIndicator = 888)
+#Generate the z design matrix for random effect
+#PR, HER2 and grade is random effect
+z.design.random <- z.standard[,2:4]
 
+#compute the score and information matrix for random effect
+score.test.random <- ScoreTestMixedModel(y=y,
+                                         x=SNP,
+                                         z.design=z.design.random,
+                                         score.test.support=score.support.random,
+                                         missingTumorIndicator=888)
+#the first element is the score
+#the second element is the information matrix
+score.random <- score.test.random[[1]]
+infor.random <- score.test.random[[2]]
 
-
-
+#after we get the the fixed effect score, infor and random effect score, infor, we can combine them through the following function. 
+#two p value will be generated.
+#the first p value for global association test.
+#the second p value is for the null hypothesis 
+#that random effect is 0
+#Under this situation, the second p value is NOT 
+#the global heterogeneity test p value since ER is fixed
+p.value.mtop <- DisplayMixedScoreTestResult(
+  score.fixed,
+  infor.fixed,
+  score.random,
+  infor.random
+)  
 
 
 
