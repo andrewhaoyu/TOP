d9b9b343b0532fd496c50ce0b4124df07dfae85f
Zhang <andrew.haoyu@gmail.com>
2022-07-10 00:14:27 -0400

    GenerateZDesignCombination

---
+++ R/GenerateZDesignCombination.R
@@ -0,0 +1,28 @@
+#' Generate the three Z design matrix given the tumor characteristics
+#'
+#' @param y the phenotype file. The first column is the case control disease status. The other columns are the tumor characteristics status
+
+#' @keywords export
+
+GenerateZDesignCombination <- function(y,
+                                       missingTumorIndicator = 888,
+                                       cutoff = 10){
+  
+  missing.data.vec <- GenerateMissingPosition(y,missingTumorIndicator)
+  y.pheno.complete <- y[-missing.data.vec,,drop=F]
+  initial.set <- InitialSetup(y.pheno.complete,
+                              baselineonly,
+                              additive,
+                              pairwise.interaction,
+                              saturated,
+                              cutoff =cutoff
+  )
+  
+  z.design.additive = initial.set[[5]]
+  z.design.pairwise.interaction = initial.set[[6]]
+  z.design.saturated = initial.set[[7]]
+  return(list(z.design.additive = z.design.additive,
+              z.design.pairwise.interaction = z.design.pairwise.interaction,
+              z.design.saturated = z.design.saturated
+  ))
+}
